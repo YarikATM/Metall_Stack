511fbb251727e97d4670591fdf7f5d35a38ac916
Yaroslav <108934904+YarikATM@users.noreply.github.com>
2024-01-19 01:29:27 +0700

    Create init_script.sh

---
+++ init_script.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Определение списка микросервисов
+SERVICES=("scraper" "tg_bot" )
+
+# Определение пути к папке проекта
+PROJECT_DIR="apps"
+
+if [ ! -d "$PROJECT_DIR" ]; then
+        mkdir "$PROJECT_DIR"
+fi
+
+# Функция для инициализации и обновления репозиториев микросервисов
+init_and_update_repositories() {
+    local repository_name=$1
+    local repository_url=$2
+
+    echo "Инициализация и обновление репозитория: $repository_name"
+    cd "$PROJECT_DIR"
+    
+    # Создание папки для микросервиса, если она не существует
+    if [ ! -d "$repository_name" ]; then
+        mkdir "$repository_name"
+    fi
+    
+    cd "$repository_name"
+
+    # Инициализация репозитория, если папка пустая
+    if [ -z "$(ls -A)" ]; then
+        git init
+        git remote add origin "$repository_url"
+        git fetch origin
+        git checkout main
+    fi
+
+    git pull 
+}
+
+# Инициализация и обновление каждого репозитория
+for service in "${SERVICES[@]}"; do
+    repository_url="git@github.com:YarikATM/$service.git"
+    init_and_update_repositories "$service" "$repository_url"
+done
+
+# Запуск docker-compose
+#echo "Запуск docker-compose"
+#cd "$PROJECT_DIR"
+#docker-compose up -d
