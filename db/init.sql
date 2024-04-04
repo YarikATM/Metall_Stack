276d35fc8812ed01cf87ae3f42ba962187f51b83
yaroslav <yaroslavpavlenko2@gmail.com>
2024-04-04 16:29:17 +0700

    fix

--- db/init.sql
+++ db/init.sql
@@ -8,3 +8,13 @@ CREATE USER 'Admin'@'%' IDENTIFIED BY '25072004Yp';
 GRANT ALL PRIVILEGES ON Metall.* TO 'Admin'@'%';
 GRANT ALL PRIVILEGES ON Tg_metall.* TO 'Admin'@'%';
 
+create table Metall.Price
+(
+    Id        int          null,
+    time      datetime     null,
+    City      varchar(155) null,
+    Company   varchar(155) null,
+    Name      varchar(100) null,
+    Price_Fiz float        null,
+    Price_Ur  float        null
+);
